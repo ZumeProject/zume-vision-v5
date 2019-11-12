a3cab567cef50ef83f28f4d7ad67b072596132ef
Chris Chasm <chris@chasm.solutions>
2019-11-12 06:21:13 -0700

    Added standard modal.

---
+++ parts/content-modal.php
@@ -0,0 +1,7 @@
+<!-- Multi-use Modal-->
+<div class="reveal" id="training-modal" data-reveal data-v-offset="10">
+    <div id="training-modal-content"><div class="loader">Loading...</div></div>
+    <button class="close-button" data-close aria-label="Close modal" type="button">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
