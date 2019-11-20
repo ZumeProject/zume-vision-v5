0f0ceddc6e84376e957aaf0831a40b129596b909
Chris Chasm <chris@chasm.solutions>
2019-11-20 14:41:35 -0700

    Added prayer template

---
+++ template-prayer.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Template Name: Prayer
+ */
+
+
+get_header(); ?>
+
+<div id="content">
+
+    <div id="main" class="cell" role="main">
+
+        <!-- Progress Section-->
+        <div class="grid-x grid-padding-x deep-blue-section">
+            <div class="cell center padding-top-1">
+                <h2>Prayer</h2>
+                <h3>Sign up to pray with us</h3>
+            </div>
+        </div>
+        <div class="grid-x blue-notch-wrapper"><div class="cell center blue-notch"></div></div>
+
+        <!-- Hero Stats-->
+        <div class="grid-x white-section">
+
+            <div class="cell small-1"></div>
+
+            <div class="cell small-10">
+
+            </div>
+
+            <div class="cell small-1"></div>
+
+        </div>
+
+    </div> <!-- end #main -->
+
+</div> <!-- end #content -->
+
+<?php //get_template_part( "parts/content", "fullmodal" ); ?>
+
+<?php get_footer(); ?>
