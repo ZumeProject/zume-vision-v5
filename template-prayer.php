94c39775fa30ddfd479ef96d5537b6a2facdd47c
Chris Chasm <chris@chasm.solutions>
2019-11-20 15:10:41 -0700

    Added prayer elements

--- template-prayer.php
+++ template-prayer.php
@@ -3,39 +3,86 @@
  * Template Name: Prayer
  */
 
-
 get_header(); ?>
 
 <div id="content">
 
     <div id="main" class="cell" role="main">
 
-        <!-- Progress Section-->
+        <!-- Prayer Header -->
         <div class="grid-x grid-padding-x deep-blue-section">
+
             <div class="cell center padding-top-1">
+
                 <h2>Prayer</h2>
-                <h3>Sign up to pray with us</h3>
+
+                <h3>Tools to Feed Extraordinary Prayer</h3>
+
             </div>
         </div>
         <div class="grid-x blue-notch-wrapper"><div class="cell center blue-notch"></div></div>
 
         <!-- Hero Stats-->
-        <div class="grid-x white-section">
+        <div class="grid-x white-section center">
 
-            <div class="cell small-1"></div>
+            <div class="cell medium-1"></div>
 
-            <div class="cell small-10">
-
+            <div class="cell medium-10">
+                <div class="grid-x grid-margin-x">
+                    <div class="cell medium-6 callout">
+                        <h3>Location Focused Prayer Subscription</h3>
+                        <p>Subscribe to prayer updates for specific regions that you care about. You can sign up for daily, weekly, or bi monthly email updates.</p>
+                    </div>
+                    <div class="cell medium-6 callout">
+                        <h3>Whatsapp Live Prayer Feed</h3>
+                        <p>Join our real time prayer feed in Whatsapp. This is a live feed of disciple making steps people are taking globally through Zume.Training. Pray real time and encourage their faith and obedience.</p>
+                        <p><a href="#" class="button secondary-button large">Join Whatsapp Live Prayer Feed</a></p>
+                        <p><a href="#" class="tiny">Get Whatsapp for your phone</a></p>
+                    </div>
+                </div>
             </div>
 
-            <div class="cell small-1"></div>
+            <div class="cell medium-1"></div>
 
         </div>
+        <div class="grid-x white-notch-wrapper"><div class="cell center white-notch"></div></div><!-- White Notch-->
+
+        <!-- World Changing Prayer Campaign-->
+        <div class="grid-x deep-blue-section center">
+            <div class="cell">
+                <h2>How to Launch a World Changing Prayer Campaign</h2>
+            </div>
+        </div>
+        <div class="grid-x blue-notch-wrapper"><div class="cell center blue-notch"></div></div>
+
+        <div class="grid-x white-section center">
+
+            <div class="cell medium-1"></div>
+
+            <div class="cell medium-10">
+                <div class="grid-x grid-margin-x">
+                    <div class="cell medium-6 callout">
+                        <h3>Location Focused Prayer Subscription</h3>
+                        <p>Subscribe to prayer updates for specific regions that you care about. You can sign up for daily, weekly, or bi monthly email updates.</p>
+                    </div>
+                    <div class="cell medium-6 callout">
+                        <h3>Whatsapp Live Prayer Feed</h3>
+                        <p>Join our real time prayer feed in Whatsapp. This is a live feed of disciple making steps people are taking globally through Zume.Training. Pray real time and encourage their faith and obedience.</p>
+                        <p><a href="#" class="button secondary-button large">Join Whatsapp Live Prayer Feed</a></p>
+                        <p><a href="#" class="tiny">Get Whatsapp for your phone</a></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="cell medium-1"></div>
+
+        </div>
+
 
     </div> <!-- end #main -->
 
 </div> <!-- end #content -->
 
-<?php //get_template_part( "parts/content", "fullmodal" ); ?>
+<?php get_template_part( "parts/content", "modal" ); ?>
 
 <?php get_footer(); ?>
