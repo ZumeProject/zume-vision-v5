e9577faeecefa676f47ee2fefcb5e164d98459e2
Chris Chasm <chris@chasm.solutions>
2019-12-21 08:44:00 -0700

    Added narrow width template

---
+++ template-narrow-width.php
@@ -0,0 +1,23 @@
+<?php
+/**
+Template Name: Narrow Width (No Sidebar)
+ */
+
+get_header(); ?>
+
+<div class="content">
+
+    <main class="main narrow-width cell padding-horizontal-1" role="main">
+
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+            <?php get_template_part( 'parts/loop', 'page' ); ?>
+
+        <?php endwhile;
+        endif; ?>
+
+    </main> <!-- end #main -->
+
+</div> <!-- end #content -->
+
+<?php get_footer(); ?>
