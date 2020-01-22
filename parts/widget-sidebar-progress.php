fffc82566e5454e0f3ca96155d00737e583ca3d7
Chris Chasm <chris@chasm.solutions>
2020-01-22 15:15:07 -0700

    Added live counters.

--- parts/widget-sidebar-progress.php
+++ parts/widget-sidebar-progress.php
@@ -2,19 +2,19 @@
 <div class="training padding-left-1">
     <p>
         Born with no access to the gospel today<br>
-        <span class="progress-counter">94,000</span>
+        <span class="progress-counter" id="births-among-unreached-today-count-1">94,000</span>
     </p>
     <p>
         Christless deaths today<br>
-        <span class="progress-counter">94,000</span>
+        <span class="progress-counter" id="christless-deaths-today-count-1">94,000</span>
     </p>
     <p>
         Trainings needed<br>
-        <span class="progress-counter">94,000</span>
+        <span class="progress-counter" id="trainings-needed-count-1">94,000</span>
     </p>
     <p>
         Churches needed<br>
-        <span class="progress-counter">94,000</span>
+        <span class="progress-counter" id="churches-needed-count-1">94,000</span>
     </p>
 
     <a href="/progress" class="button primary-button-hollow">See Progress</a>
