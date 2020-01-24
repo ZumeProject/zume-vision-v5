afc438844405884b8151a8b430aa107451b092ec
Chris Chasm <chris@chasm.solutions>
2020-01-24 14:57:33 -0700

    Removed tags from Reports

--- functions/post-type-reports.php
+++ functions/post-type-reports.php
@@ -175,33 +175,7 @@ class Zume_Reports_Post_Type
     public function create_tag_taxonomies()
     {
         // Add new taxonomy, NOT hierarchical (like tags)
-        $labels = array(
-            'name' => _x( 'Report Tags', 'taxonomy general name' ),
-            'singular_name' => _x( 'Tag', 'taxonomy singular name' ),
-            'search_items' =>  __( 'Search Tags' ),
-            'popular_items' => __( 'Popular Tags' ),
-            'all_items' => __( 'All Tags' ),
-            'parent_item' => null,
-            'parent_item_colon' => null,
-            'edit_item' => __( 'Edit Tag' ),
-            'update_item' => __( 'Update Tag' ),
-            'add_new_item' => __( 'Add New Tag' ),
-            'new_item_name' => __( 'New Tag Name' ),
-            'separate_items_with_commas' => __( 'Separate tags with commas' ),
-            'add_or_remove_items' => __( 'Add or remove tags' ),
-            'choose_from_most_used' => __( 'Choose from the most used tags' ),
-            'menu_name' => __( 'Tags' ),
-        );
 
-        register_taxonomy('report_tag','reports',array(
-            'hierarchical' => false,
-            'labels' => $labels,
-            'show_ui' => true,
-            'update_count_callback' => '_update_post_term_count',
-            'query_var' => true,
-            'rewrite' => array( 'slug' => 'report-tag' ),
-            'show_in_rest' => true,
-        ));
         register_taxonomy('report_categories','reports',array(
             'hierarchical' => true,
             'labels' => [
