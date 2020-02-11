e87786134f0d70e370f17b6adc920ad4c07436a3
Chris Chasm <chris@chasm.solutions>
2020-02-11 16:25:35 -0700

    Removed dates on recent playbooks

--- parts/widget-sidebar-recent-playbook.php
+++ parts/widget-sidebar-recent-playbook.php
@@ -7,10 +7,9 @@
         'post_status' => 'publish'
     ] );
     foreach ( $recent_posts as $recent ) {
-        printf( '<li><a href="%1$s">%2$s (%3$s)</a></li>',
+        printf( '<li><a href="%1$s">%2$s</a></li>',
             esc_url( get_permalink( $recent['ID'] ) ),
-            apply_filters( 'the_title', $recent['post_title'], $recent['ID'] ),
-            date( 'm-d', strtotime( $recent['post_date_gmt'] ) )
+            apply_filters( 'the_title', $recent['post_title'], $recent['ID'] )
         );
     }
     ?>
