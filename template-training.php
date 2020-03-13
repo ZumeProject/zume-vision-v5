f73afc9f5c5b70b2831bc9e33dd55f1d9021a46f
Chris Chasm <chris@chasm.solutions>
2020-03-13 14:25:29 -0600

    Upgrades to the Zume Training page

--- template-training.php
+++ template-training.php
@@ -6,24 +6,29 @@ Template Name: Training
 
 <?php get_header(); ?>
 
+<!-- Title Section-->
+<div class="grid-x grid-padding-x deep-blue-section padding-vertical-1">
+    <div class="cell center">
+        <h1 class="center title">Training</h1>
+    </div>
+</div>
+<div class="grid-x blue-notch-wrapper"><div class="cell center blue-notch"></div></div>
+
 <div class="page-content">
     <!-- Challenge -->
     <div class="grid-x grid-padding-x padding-top-1">
-        <div class="cell center" >
-            <h1 style="text-align:center !important; font-weight: 100; font-size: 3.5rem; color: #323a68;" class="padding-bottom-1"><?php esc_html_e( 'Zúme Training', 'zume' ) ?></h1>
-            <p style="max-width:600px;margin:0 auto;">Zúme Training uses an online platform to equip participants in basic disciple-making and simple church planting multiplication principles, processes, and practices. The course is online, free, and self-led for groups of 3 - 12.</p>
-
-        </div>
-        <div class="small-6 small-centered cell video-section" style="max-width:400px;">
-            <iframe style="border: 1px solid lightgrey;"  src="https://player.vimeo.com/video/248149797" width="560" height="215"
-                    frameborder="1"
-                    webkitallowfullscreen mozallowfullscreen allowfullscreen>
-            </iframe>
-        </div>
-
         <div class="cell center">
+            <figure class="wp-block-image size-large">
+                <img src="https://zume.vision/wp-content/uploads/sites/12/2020/03/zume-training-logo.png" style="max-width:400px;" alt="zume training" class="wp-image-427" srcset="https://zume.vision/wp-content/uploads/sites/12/2020/03/zume-training-logo.png 911w, https://zume.vision/wp-content/uploads/sites/12/2020/03/zume-training-logo-300x67.png 300w, https://zume.vision/wp-content/uploads/sites/12/2020/03/zume-training-logo-768x171.png 768w" sizes="(max-width: 911px) 100vw, 911px">
+            </figure>
+            <p style="max-width:600px;margin:0 auto;">Zúme Training uses an online platform to equip participants in basic disciple-making and simple church planting multiplication principles, processes, and practices. The course is online, free, and self-led for groups of 3 - 12.</p>
+            <div class="video-section" style="max-width:400px;">
+                <iframe style="border: 1px solid lightgrey;"  src="https://player.vimeo.com/video/248149797" width="560" height="215"
+                        frameborder="1"
+                        webkitallowfullscreen mozallowfullscreen allowfullscreen>
+                </iframe>
+            </div>
             <a href="https://zume.training" class="button large secondary-button" >Get Started</a>
-            <a href="https://zume.vision/articles/zume-training-course-outline/" class="button large secondary-button">See the Course Outline</a>
         </div>
     </div>
 </div>
@@ -51,9 +56,6 @@ Template Name: Training
                     </div>
                 </div>
             </div>
-            <div class="cell center">
-                <a href="https://zume.training" target="_blank" class="button large secondary-button" >Explore the Training</a>
-            </div>
         </div>
         <div class="medium-2 small-1 cell"></div>
     </div>
@@ -62,10 +64,10 @@ Template Name: Training
 <div class="page-content">
     <!-- Slider -->
     <div class="grid-x grid-padding-x">
-        <div class="cell">
-            <h2 class="center">What others are saying?</h2>
+        <div class="cell ">
+            <h2 class="center padding-bottom-2">What others are saying?</h2>
         </div>
-        <div class="cell front-page-social" data-equalizer style="color:#21336A;">
+        <div class="cell front-page-social" data-equalizer >
             <div class="grid-x grid-margin-x grid-margin-y">
                 <div class="large-4 medium-6 small-12 cell centered" data-equalizer-watch>
                     <img src="/wp-content/themes/zume-training/assets/images/home/1body.jpg"
@@ -95,7 +97,7 @@ Template Name: Training
                          class="center front-page-social-image" style="max-height: 60px">
 
                     <p class="text-center" style="color:#21336A">
-                        "The principles and life practices packed into the Zúme disciple-multiplication training course have enormous potential to impact not only the USA but also, as the course is translated into 34 other languages, the world as well."
+                        "The principles and life practices packed into the Zúme training course have enormous potential to impact, not only the USA, but the world as well."
                     </p>
                 </div>
                 <div class="large-4 medium-6 small-12 cell centered" data-equalizer-watch>
@@ -110,12 +112,51 @@ Template Name: Training
             </div>
         </div>
         <div class="cell center">
-            <a href="https://zume.training/training" class="button centered large secondary-button" >Explore the Training</a>
+            <a href="https://zume.training/training" class="button centered large secondary-button" >Get Started</a>
         </div>
 
     </div> <!-- End cell-->
 </div>
 <hr>
+<style>
+#outline h3 { font-weight: bold;}
+#outline li a { font-weight: bold;}
+</style>
+<div class="page-content" style="max-width: 700px;">
+    <section id="outline" class="entry-content" itemprop="text">
+        <h2 class="center padding-bottom-2">Zúme Training Course Outline</h2>
+
+        <h3>Session 1</h3>
+        <ul><li><a href="https://zume.training/god-uses-ordinary-people">God Uses Ordinary People</a><br>You’ll see how God uses ordinary people doing simple things to make a big impact.</li><li><a href="https://zume.training/definition-of-disciple-and-church">Simple Definition of Disciple and Church</a><br>Discover the essence of being a disciple, making a disciple, and what is the church.</li><li><a href="https://zume.training/spiritual-breathing-is-hearing-and-obeying-god">Spiritual Breathing is Hearing and Obeying God</a><br>Being a disciple means we hear from God and we obey God.</li><li><a href="https://zume.training/soaps-bible-reading">SOAPS Bible Reading</a><br>A tool for daily Bible study that helps you understand, obey, and share God’s Word.</li><li><a href="https://zume.training/accountability-groups">Accountability Groups</a><br>A  tool for two or three people of the same gender to meet weekly and  encourage each other in areas that are going well and reveal areas that  need correction.</li></ul>
+
+        <h3>Session 2</h3>
+        <ul><li><a href="https://zume.training/consumer-vs-producer-lifestyle">Consumer vs Producer Lifestyle</a><br>You’ll discover the four main ways God makes everyday followers more like Jesus.</li><li><a href="https://zume.training/how-to-spend-an-hour-in-prayer">How to Spend an Hour in Prayer</a><br>See how easy it is to spend an hour in prayer.</li><li><a href="https://zume.training/relational-stewardship-list-of-100">Relational Stewardship – List of 100</a><br>A tool designed to help you be a good steward of your relationships.</li></ul>
+
+        <h3>Session 3</h3>
+        <ul><li><a href="https://zume.training/the-kingdom-economy">The Kingdom Economy</a><br>Learn  how God’s economy is different from the world’s. God invests more in  those who are faithful with what they’ve already been given.</li><li><a href="https://zume.training/the-gospel-and-how-to-share-it">The Gospel and How to Share It</a><br>Learn a way to share God’s Good News from the beginning of humanity all the way to the end of this age.</li><li><a href="https://zume.training/baptism-and-how-to-do-it">Baptism and How To Do It</a><br>Jesus  said, “Go and make disciples of all nations, BAPTIZING them in the name  of the Father and of the Son and of the Holy Spirit…” Learn how to put  this into practice.</li></ul>
+
+        <h3>Session 4</h3>
+        <ul><li><a href="https://zume.training/prepare-your-3-minute-testimony">Prepare Your 3-Minute Testimony</a><br>Learn how to share your testimony in three minutes by sharing how Jesus has impacted your life.</li><li><a href="https://zume.training/vision-casting-the-greatest-blessing">Vision Casting the Greatest Blessing</a><br>Learn  a simple pattern of making not just one follower of Jesus but entire  spiritual families who multiply for generations to come.</li><li><a href="https://zume.training/duckling-discipleship-leading-sooner">Duckling Discipleship – Leading Immediately</a><br>Learn what ducklings have to do with disciple-making.</li><li><a href="https://zume.training/eyes-to-see-where-the-kingdom-isnt">Eyes to See Where The Kingdom Isn’t</a><br>Begin to see where God’s Kingdom isn’t. These are usually the places where God wants to work the most.</li><li><a href="https://zume.training/the-lords-supper-and-how-to-lead-it">The Lord’s Supper and How To Lead It</a><br>It’s a simple way to celebrate our intimate connection and ongoing relationship with Jesus. Learn a simple way to celebrate.</li></ul>
+
+        <h3>Session 5</h3>
+        <ul><li><a href="https://zume.training/prayer-walking">Prayer Walking and How To Do It</a><br>It’s  a simple way to obey God’s command to pray for others. And it’s just  what it sounds like — praying to God while walking around!</li><li><a href="https://zume.training/a-person-of-peace-and-how-to-find-one">A Person of Peace and How To Find One</a><br>Learn who a person of peace might be and how to know when you’ve found one.</li><li><a href="https://zume.training/the-bless-prayer-pattern">The BLESS Prayer Pattern</a><br>Practice a simple mnemonic to remind you of ways to pray for others.</li></ul>
+
+        <h3>Session 6</h3>
+        <ul><li><a href="https://zume.training/faithfulness-is-better-than-knowledge">Faithfulness is Better Than Knowledge</a><br>It’s important what disciples know — but it’s much more important what they DO with what they know.</li><li><a href="https://zume.training/3-3-group-meeting-pattern">3/3 Group Meeting Pattern</a><br>A  3/3 Group is a way for followers of Jesus to meet, pray, learn, grow,  fellowship and practice obeying and sharing what they’ve learned. In  this way, a 3/3 Group is not just a small group but a Simple Church.</li></ul>
+
+        <h3>Session 7</h3>
+        <ul><li><a href="https://zume.training/training-cycle-for-maturing-disciples">Training Cycle for Maturing Disciples</a><br>Learn the training cycle and consider how it applies to disciple making.</li></ul>
+
+        <h3>Session 8</h3>
+        <ul><li><a href="https://zume.training/leadership-cells">Leadership Cells</a><br>A Leadership Cell is a way someone who feels called to lead can develop their leadership by practicing serving.</li></ul>
+
+        <h3>Session 9</h3>
+        <ul><li><a href="https://zume.training/expect-non-sequential-growth">Expect Non-Sequential Growth</a><br>See how disciple making doesn’t have to be linear. Multiple things can happen at the same time.</li><li><a href="https://zume.training/pace-of-multiplication-matters">Pace of Multiplication Matters</a><br>Multiplying matters and multiplying quickly matters even more. See why pace matters.</li><li><a href="https://zume.training/always-part-of-two-churches">Always Part of Two Churches</a><br>Learn how to obey Jesus’ commands by going AND staying.</li><li><a href="https://zume.training/three-month-plan">Three-Month Plan</a><br>Create and share your plan for how you will implement the Zúme tools over the next three months.</li></ul>
+
+        <h3>Session 10</h3>
+        <ul><li><a href="https://zume.training/coaching-checklist">Coaching Checklist</a><br>A powerful tool you can use to quickly assess your own strengths and  vulnerabilities when it comes to making disciples who multiply.</li><li><a href="https://zume.training/leadership-in-networks">Leadership in Networks</a><br>Learn how multiplying churches stay connected and live life together as an extended, spiritual family.</li><li><a href="https://zume.training/peer-mentoring-groups">Peer Mentoring Groups</a><br>This  is a group that consists of people who are leading and starting 3/3  Groups. It also follows a 3/3 format and is a powerful way to assess the  spiritual health of God’s work in your area.</li><li><a href="https://zume.training/four-fields-tool">Four Fields Tool</a><br>The  four fields diagnostic chart is a simple tool to be used by a  leadership cell to reflect on the status of current efforts and the  kingdom activity around them.</li><li><a href="https://zume.training/generational-mapping">Generational Mapping</a><br>Generation mapping is another simple tool to help leaders in a movement understand the growth around them.</li></ul>
+    </section>
+</div>
 
 
 <?php get_footer(); ?>
