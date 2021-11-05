d719c6a2834f9e2d65de263b590f38132e405dc3
Chris Chasm <chris@chasm.solutions>
2021-11-05 14:21:33 -0600

    Podcast

--- single-category-podcasts.php
+++ single-category-podcasts.php
@@ -22,7 +22,7 @@ get_header();
 
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-                <?php get_template_part( 'parts/loop', 'podcast' ); ?>
+                <?php get_template_part( 'parts/loop', 'single' ); ?>
 
             <?php endwhile; else : ?>
 
