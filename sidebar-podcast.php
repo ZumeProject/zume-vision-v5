b0ca7acf957c1ac6d54f6f410cc9806b42e26eea
Chris Chasm <chris@chasm.solutions>
2021-11-08 09:21:29 -0700

    Podcast edits

--- sidebar-podcast.php
+++ sidebar-podcast.php
@@ -7,7 +7,7 @@
 <div id="sidebar2" class="sidebar small-12 medium-3 large-3 cell" role="complementary">
 
     <div class="padding-horizontal-1">
-        <h3>Description</h3>
+        <h3>Show Description</h3>
         <p>Zúme is a community of practice for those who want to see disciple making movements. Our vision is to saturate the world with multiplying disciples in our generation. This podcast interviews the practitioners of the community around the world.</p>
     </div>
     <hr>
@@ -36,8 +36,6 @@
     <!-- subscribe section-->
     <?php get_template_part( "parts/content", "join" ); ?>
 
-    <div class="padding-horizontal-1">
-        <a class="button secondary-button large expanded" href="/subscribe-to-podcast/">Subscribe to Podcast</a>
-    </div>
+    <a class="button secondary-button large expanded" href="/subscribe-to-podcast/">Subscribe to Podcast</a>
 
 </div>
