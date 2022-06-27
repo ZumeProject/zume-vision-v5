be5c8a04f4e455f605dcd81c3e6fe23745d1e521
Chris Chasm <chris@chasm.solutions>
2022-06-27 14:02:51 -0600

    Fix podcast

--- sidebar-podcast.php
+++ sidebar-podcast.php
@@ -10,26 +10,7 @@
         <h3>Show Description</h3>
         <p>Zúme is a community of practice for those who want to see disciple making movements. Our vision is to saturate the world with multiplying disciples in our generation. This podcast interviews the practitioners of the community around the world.</p>
     </div>
-    <hr>
 
-    <div class="padding-horizontal-1">
-        <h3><a href="/multiplying-disciples-podcast/">Recent Episodes</a></h3>
-        <ol style="padding-left: 1.5em; text-indent:-1.5em;">
-            <?php
-            $recent_posts = wp_get_recent_posts( [
-                'post_type' => 'post',
-                'numberposts'  => 10,
-                'post_status' => 'publish',
-            ] );
-            foreach ( $recent_posts as $recent ) {
-                printf( '<li><a href="%1$s">%2$s </a></li>',
-                    esc_url( get_permalink( $recent['ID'] ) ),
-                    apply_filters( 'the_title', $recent['post_title'], $recent['ID'] )
-                );
-            }
-            ?>
-        </ol>
-    </div>
 
     <hr>
 
@@ -41,10 +22,26 @@
 
     <?php get_template_part( "parts/content", "join" ); ?>
 
-    <?php get_template_part( 'parts/widget', 'sidebar-seo-links' ); ?>
 
     <hr>
 
-    <?php get_template_part( "parts/content", "reports-subscribe" ); ?>
+    <div class="padding-horizontal-1">
+        <h3><a href="/multiplying-disciples-podcast/">All Episodes</a></h3>
+        <ol style="padding-left: 1.5em; text-indent:-1.5em;">
+            <?php
+            $recent_posts = wp_get_recent_posts( [
+                'post_type' => 'post',
+                'numberposts'  => -1,
+                'post_status' => 'publish',
+            ] );
+            foreach ( $recent_posts as $recent ) {
+                printf( '<li><a href="%1$s">%2$s </a></li>',
+                    esc_url( get_permalink( $recent['ID'] ) ),
+                    apply_filters( 'the_title', $recent['post_title'], $recent['ID'] )
+                );
+            }
+            ?>
+        </ol>
+    </div>
 
 </div>
