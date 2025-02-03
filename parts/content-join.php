080fad324e65028a5d8256dd54f5f3f1b5702c60
Chris Chasm <chris@chasm.solutions>
2025-02-03 09:11:05 -0700

    join button

--- parts/content-join.php
+++ parts/content-join.php
@@ -1,11 +1,5 @@
 <div id="join">
-    <div class="join-wrapper"><a href="https://zume.training/wizard/join-the-community-vision" target="_blank" class="button secondary-button large expanded join-the-community" type="button">Join Our Community</a></div>
-    <div class="reveal large" id="join-modal" data-reveal data-v-offset="10">
-        <div id="modal-content"><div class="loader">Loading...</div></div>
-        <button class="close-button" data-close aria-label="Close modal" type="button">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
+    <div class="join-wrapper"><a href="https://zume.training/wizard/join-the-community-vision" target="_blank" class="button secondary-button large expanded " type="button">Join Our Community</a></div>
 </div>
 
 
